ed3a592653e60d16a510be084b9b541d1b6e8b38
AZMSHA <azmsha2003@gmail.com>
2024-07-08 23:37:14 +0500

    fetched data and used redux, created file structure and added dependencies as expected by wordpress site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useEffect } from "react";
+import "./App.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case "setGoogleReviews":
+      return { ...state, googleReviews: action.payload };
+    case "setTrustPilotReviews":
+      return { ...state, trustPilotReviews: action.payload };
+    case "setBoth":
+      return {
+        ...state,
+        googleReviews: action.payload.googleReviews,
+        trustPilotReviews: action.payload.trustPilotReviews,
+      };
+    default:
+      throw new Error();
+  }
 }
 
-export default App
+function App() {
+  const [reviews, setReviews] = useReducer(reducer, {
+    googleReviews: [],
+    trustPilotReviews: [],
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      const googleReviewsUrl = "/google-reviews.json";
+      const trustPilotReviewsUrl = "/trustpilot-reviews.json";
+
+      try {
+        const [googleRes, trustPilotRes] = await Promise.all([
+          fetch(googleReviewsUrl),
+          fetch(trustPilotReviewsUrl),
+        ]);
+
+        const googleReviews = await googleRes.json();
+        const trustPilotReviews = await trustPilotRes.json();
+
+        setReviews({
+          type: "setBoth",
+          payload: { googleReviews, trustPilotReviews },
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <></>;
+}
+
+export default App;
